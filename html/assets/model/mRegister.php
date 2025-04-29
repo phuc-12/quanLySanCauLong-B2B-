3e64733370da94b632cad5b9314437891c46a087
phuc-12 <phucqua147@gmail.com>
2025-04-29 13:48:12 +0700

    dk

--- html/assets/model/mRegister.php
+++ html/assets/model/mRegister.php
@@ -2,19 +2,33 @@
     include_once('ketnoi.php');
     
     class mRegiter{
-        public function mRegister1User($username,$pw,$email,$trangthai,$loaiND,$sdt){
+        public function mRegister1User($username,$pw,$email,$loaiND,$sdt){
             $p = new clsKetNoi;
             $con = $p -> moketnoi();
-            $loaiND = 1;
             $trangthai = 1;
             // $pw = MD5($pw);      
-            $sql = "INSERT INTO taikhoannguoidung (`username` ,`passwords` ,`email` ,`trangThai` ,`loaiNguoiDung` ,`soDienThoai`) VALUES ('$username', '$pw', '$email', '$trangthai', '$loaiND', '$sdt')";
+            $sql = "INSERT INTO taikhoannguoidung (`username` ,`passwords` ,`email` ,`trangthai` ,`loainguoidung` ,`sodienthoai`) VALUES ('$username', '$pw', '$email', '$trangthai', '$loaiND', '$sdt')";
             $kq = mysqli_query($con,$sql);
             $p -> dongKetNoi($con);
             return $kq;
             
         }
         
+        public function selectAllUser($username){
+            $p = new clsKetNoi();
+            $con = $p -> moKetNoi();
+            $sql = "SELECT * FROM taikhoannguoidung where username = '$username'" ;
+            $kq = mysqli_query($con,$sql);
+            $p -> dongKetNoi($con);
+            if($kq->num_rows>0)
+            {
+                return 0;
+            }
+            else 
+            {
+                return 1;
+            }
+        }
     }
 
 ?>
