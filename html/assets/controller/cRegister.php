ff8d2fcfc212f3606c0cd13d34b8678dd6599675
phuc-12 <phucqua147@gmail.com>
2025-05-09 13:23:37 +0700

    xong update_tn kh - tạo tk-tạo kh, update csdl

--- html/assets/controller/cRegister.php
+++ html/assets/controller/cRegister.php
@@ -2,23 +2,22 @@
     include($_SERVER['DOCUMENT_ROOT'] . "/quanLySanCauLong-B2B-/html/assets/model/mRegister.php");
     // include_once('../model/mUser.php');
     class cRegister{
-        public function cRegister1User($username,$pw,$email,$loaiND,$sdt){
+        public function cRegister1User($username, $tenKH, $pw, $email, $loaiND, $sdt) {
             $pp = new mRegiter();
-            $ktra = $pp -> selectAllUser($username);
-
-            if($ktra == 1)
-            {
-                $pp->mRegister1User($username,$pw,$email,$loaiND,$sdt);
-                echo'<script language="javascript">
-							window.location="login.php";
-							</script>';
-            }
-            else 
-            {
+            $ktra = $pp->selectAllUser($username);
+        
+            if ($ktra == 1) {
+                $idND = $pp->mRegister1User($username, $pw, $email, $loaiND, $sdt); // nhận id vừa tạo
+                if ($idND > 0) {
+                    $pp->mRegister1KH($tenKH, $sdt, $email, $idND); // truyền id này vào khách hàng
+                }
+                echo "<script>alert('Đăng ký thành công!'); window.location.href='login.php';</script>";
+            } else {
                 return -1;
             }
-          
-    }
+        }
+        
+        
         public function getTKND($username,$pass)
         {
             $p = new mRegiter();
